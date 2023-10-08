d6f24e02e33744edba337707010ff67d478f8894
Joshua_Ajagbe <joshua@equally.ai>
2023-10-08 16:57:55 +0100

    initial setup

---
+++ apps/frontend/src/app/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './styles.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="container">
+      <h1>Calculator App</h1>
+    </div>
+  );
+}
+
+export default App;
